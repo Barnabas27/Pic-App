154160e017cd7578b50193409cb5ea832d4b2fc4
Barnabas27 <bnyambura032@gmail.com>
2021-01-10 20:41:38 +0300

    add tests to the models Location and Category

--- pics/tests.py
+++ pics/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
 
+from .models import Location,Category,Pic
+
 # Create your tests here.
+
+class LocationTestClass(TestCase):
+    
+    def setUp(self):
+        self.Canada = Location(location = 'Canada')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Canada,Location))
+        
+    def test_save_method(self):
+        self.Canada.save()
+        location = Location.objects.all()
+        self.assertTrue(len(location)>0)
+        
+class CategoryTestClass(TestCase):
+    
+    def setUp(self):
+        self.Nature = Category(category = 'Nature')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Nature,Category))
+        
+    def test_save_method(self):
+        self.Nature.save()
+        category = Category.objects.all()
+        self.assertTrue(len(category)>0)
