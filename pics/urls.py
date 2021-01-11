0326368ccae545fa8f3544a23027f49734e140b0
Barnabas27 <bnyambura032@gmail.com>
2021-01-12 01:02:37 +0300

    add path as well as import views

--- pics/urls.py
+++ pics/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 from . import views
 
 urlpatterns=[
-    path('',views.welcome,name='welcome'),
+    path('',views.home,name='home'),
+    path('search/',views.search_results,name='search_results'),
+    path('location/',views.location,name = 'location')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
